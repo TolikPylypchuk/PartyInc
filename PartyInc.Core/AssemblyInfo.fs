859ce85720ab1057ff0a40e005b2d65bbdffbbe8
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-12-03 14:25:15 +0200

    Add PartyInc.Core, README and LICENSE

---
+++ PartyInc.Core/AssemblyInfo.fs
@@ -0,0 +1,23 @@
+﻿namespace PartyInc.Core.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("PartyInc.Core")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Party Inc Core")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("4a620780-0bd5-4eed-b7bf-0f1a3b1a101e")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
