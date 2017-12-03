29839f0172e66c5f52e7d69f9c1076127526a618
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-12-03 17:02:15 +0200

    Add basic domain types

---
+++ PartyInc.Core/Domain.fs
@@ -0,0 +1,39 @@
+﻿namespace PartyInc.Core
+
+open System
+
+type Cake = {
+    Name: string
+    Ingredients: string list
+    Price: decimal
+}
+
+type Sweet = {
+    Name: string
+    Weight: float
+    Price: decimal
+}
+
+type Food = {
+    Cake: Cake
+    Cookies: Sweet list
+    Candies: Sweet list
+}
+
+type Drink = {
+    Name: string
+    Ingredients: string list
+    Amount: int
+    Price: decimal
+    Volume: float
+}
+
+type Order = {
+    DateTime: DateTime
+    Address: string
+    Type: string
+    MinAge: int
+    MaxAge: int
+    Food: Food
+    Drinks: Drink list
+}
---
+++ PartyInc.Core/PrologInterop.fs
@@ -0,0 +1,17 @@
+﻿module PartyInc.Core.PrologInterop
+
+open Chessie.ErrorHandling
+
+open Prolog
+
+let getSolutions (prolog : PrologEngine) file query =
+    let solutions = prolog.GetAllSolutions(file, query)
+
+    if solutions.Success
+    then solutions.NextSolution |> Seq.toList |> ok
+    else solutions.ErrMsg |> fail
+
+let getVariables (solution : Solution) =
+    solution.NextVariable
+    |> Seq.filter (fun var -> var.Name <> var.Value)
+    |> Seq.toList
