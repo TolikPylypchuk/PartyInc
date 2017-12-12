74cc5664e0aef2cd5ebd687ae0c82b437df6be2f
NataliaSlobodianiuk <slo0nata@gmail.com>
2017-12-13 01:33:57 +0200

    Merge commit

---
+++ PartyInc.Core/data/food_queries.pl
@@ -0,0 +1,8 @@
+﻿getCandyByPriceMoreEqualThan(50, candy(Name, Price)).
+getCandyByPriceLessThan(50, candy(Name, Price)).
+getCookieByPriceMoreEqualThan(50, cookie(Name, Price)).
+getCookieByPriceLessThan(50, cookie(Name, Price)).
+getCakeByPriceMoreEqualThan(50, cake(Name, Ingredients, Price)).
+getCakeByPriceLessThan(50, cake(Name, Ingredients, Price)).
+getCakeByIngredientsPresent(["ingredient1", "ingredient2", ...], cake(Name, Ingredients, Price)).
+getCakeByIngredientsAbsent(["ingredient1", "ingredient2", ...], cake(Name, Ingredients, Price)).
