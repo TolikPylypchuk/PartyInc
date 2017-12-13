b230017d721cdb4eddb7a60749d612392ec5941d
NataliaSlobodianiuk <slo0nata@gmail.com>
2017-12-13 17:09:09 +0200

    Add response variants

--- PartyInc.Core/LuisBot.fs
+++ PartyInc.Core/LuisBot.fs
@@ -2,14 +2,8 @@
 
 [<AbstractClass>]
 type LuisBot(id: string, subscriptionKey: string) = class
-    let mutable _responseVariants: Map<string, List<string>> = Map.empty
-
     member this.Id = id
     member this.SubscriptionKey = subscriptionKey
-
-    member this.ResponseVariants
-        with get() = _responseVariants
-        and set(value: Map<string, List<string>>) = _responseVariants <- value
     
     abstract member Response: string -> string
 end
--- PartyInc/MainWindow.xaml.cs
+++ PartyInc/MainWindow.xaml.cs
@@ -19,8 +19,15 @@ namespace PartyInc
             bot = new SweetsOrderConsultantBot.SweetsOrderConsultantBot(
                 Settings.Default.SweetsOrderConsultantId,
                 Settings.Default.SweetsOrderConsultantSubscriptionKey);
-
-            this.test.Text = bot.Response("Order a cake"); //I think, about 1.5 kg
+            
+            this.test.Text = bot.Response("Hi!");
+            this.test.Text = bot.Response("Order a cake");
+            this.test.Text = bot.Response("Yes, I like bananas and strawberry.");
+            this.test.Text = bot.Response("Yes, I'm allergic to chocolate.");
+            this.test.Text = bot.Response("Maybe, 3 killos.");
+            this.test.Text = bot.Response("I want it to be in range between $50 and $100.");
+            //this.test.Text = bot.Response("I think, about 1.5 kg")
+            this.test.Text = bot.Response("No, that's it.");
         }
     }
 }
